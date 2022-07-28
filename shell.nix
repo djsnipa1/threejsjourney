0617da126431aaddaddf548034bb6142b0492983
Chad Boyce <chadboyce@gmail.com>
2022-07-27 22:01:28 -0400

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -6,6 +6,9 @@ mkShell {
   # Package names can be found via https://search.nixos.org/packages
   nativeBuildInputs = [
     direnv
+    neovim
+    nodejs-18_x
+      nodePackages.pnpm
   ];
 
   NIX_ENFORCE_PURITY = true;
