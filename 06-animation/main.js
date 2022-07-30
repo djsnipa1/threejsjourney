2942d9245c3a444679a078fce65c443f76bb023f
Chad Boyce <chadboyce@gmail.com>
2022-07-30 08:42:39 +0000

    animations

--- 06-animation/main.js
+++ 06-animation/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import 'modern-css-reset/dist/reset.min.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,8 +20,8 @@ const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 const cubeMaterial = new THREE.MeshBasicMaterial({
   color: '#ff0000'
 })
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
+const mesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
@@ -32,4 +33,33 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Clock
+// const clock = new THREE.Clock()
+
+// gsap
+console.log(gsap)
+gsap.to(mesh.position, { duration: 3, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 3, x: -2 })
+
+// Animations
+const tick = () => {
+  // Clock
+  // const elapsedTime = clock.getElapsedTime()
+
+  // Update Objects
+  // camera.position.y = Math.sin(elapsedTime)
+  // camera.position.x = Math.cos(elapsedTime)
+  // camera.lookAt(mesh.position)
+
+  // mesh.rotation.x += 0.5
+
+  // Render
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(tick)
+}
+
+tick()
+
+
