ed59a7cf8f5159d15e5d3677f030e32b1f91723b
Chad Boyce <chadboyce@gmail.com>
2022-08-05 14:13:20 +0000

    start lesson 12

--- 12-materials/main.js
+++ 12-materials/main.js
@@ -1,35 +1,15 @@
 import './style.css'
-import 'modern-css-reset/dist/reset.min.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap'
-import * as dat from 'dat.gui'
-
 
 /**
  * Base
  */
-const parameters = {
-    color: 0xff0000,
-    spin: () =>
-    {
-        gsap.to(mesh.rotation, 1, { y: mesh.rotation.y + Math.PI * 2 })
-    }
-}
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 /**
  * Sizes
@@ -59,7 +39,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -74,28 +56,6 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Debug dat.gui
- */
-const gui = new dat.GUI({
-    width: 200,
-    closed: true,
-    hidden: false
-})
-// gui.hide()
-gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01).name('y axis')
-gui.add(mesh, 'visible').name('visible')
-gui.add(material, 'wireframe')
-
-gui
-    .addColor(parameters, 'color')
-    .onChange(() =>
-    {
-        material.color.set(parameters.color)
-    })
-
-gui.add(parameters, 'spin')
 
 /**
  * Animate
@@ -117,5 +77,3 @@ const tick = () =>
 }
 
 tick()
-
-
