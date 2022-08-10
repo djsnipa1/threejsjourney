8ba036d2cfbe2f1ae889128222e27122c69d66b7
Chad Boyce <chadboyce@gmail.com>
2022-08-10 18:59:39 +0000

    finished lesson 13

--- 13-text/main.js
+++ 13-text/main.js
@@ -1,7 +1,9 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "dat.gui";
+import * as dat from "lil-gui";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 
 //      _________
 //     / ======= \
@@ -18,41 +20,7 @@ import * as dat from "dat.gui";
 /**********************************************************/
 /*                       D E B U G                        */
 /**********************************************************/
-const gui = new dat.GUI();
-
-/**********************************************************/
-/*                    T E X T U R E S                     */
-/**********************************************************/
-const textureLoader = new THREE.TextureLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-
-const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
-const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
-const doorAmbientOcclustionTexture = textureLoader.load(
-  "/textures/door/ambientOcclusion.jpg"
-);
-const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
-const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
-const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
-const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
-
-// Uncomment to show Maria + line 56
-// const matcapTexture = textureLoader.load('/textures/matcaps/7.png')
-
-// Uncomment to make cool "toon" effect + line 68
-// const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
-// gradientTexture.minFilter = THREE.NearestFilter
-// gradientTexture.magFilter = THREE.NearestFilter
-// gradientTexture.generateMipmaps = false
-
-const environmentMapTexture = cubeTextureLoader.load([
-  "/textures/environmentMaps/4/px.png",
-  "/textures/environmentMaps/4/nx.png",
-  "/textures/environmentMaps/4/py.png",
-  "/textures/environmentMaps/4/ny.png",
-  "/textures/environmentMaps/4/pz.png",
-  "/textures/environmentMaps/4/nz.png",
-]);
+// const gui = new dat.GUI();
 
 /**********************************************************/
 /*                        B A S E                         */
@@ -64,113 +32,65 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**********************************************************/
-/*                     O B J E C T S                      */
+/*                    T E X T U R E S                     */
 /**********************************************************/
-// Material
-// MeshBasicMaterial
-// const material = new THREE.MeshBasicMaterial()
-// material.map = doorColorTexture;
-// // material.color.set('yellow')
-// // material.wireframe = true
-// // material.opacity = 0.2
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-// material.side = THREE.FrontSide
-
-// const material = new THREE.MeshNormalMaterial()
-// material.flatShading = true
-
-// Uncomment to show Maria
-// const material = new THREE.MeshMatcapMaterial()
-// material.matcap = matcapTexture
-
-// const material = new THREE.MeshDepthMaterial()
-
-// const material = new THREE.MeshLambertMaterial()
-
-// const material = new THREE.MeshPhongMaterial()
-// material.shininess = 100
-// material.specular = new THREE.Color(0x1188ff
-
-// Uncomment for "toon" material
-// const material = new THREE.MeshToonMaterial()
-// material.gradientMap = gradientTexture
-
-// Standard Material
-// const material = new THREE.MeshStandardMaterial();
-// material.metalness = 0;
-// material.roughness = 1;
-// material.map = doorColorTexture;
-// material.aoMap = doorAmbientOcclustionTexture;
-// material.aoMapIntensity = 1;
-// material.displacementMap = doorHeightTexture;
-// material.displacementScale = 0.05;
-// material.metalnessMap = doorMetalnessTexture;
-// material.roughnessMap = doorRoughnessTexture;
-// material.normalMap = doorNormalTexture
-// material.normalScale.set(0.5,0.5)
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-
-// Environment Map
-const material = new THREE.MeshStandardMaterial();
-material.metalness = 0.7;
-material.roughness = 0.2;
-material.envMap = environmentMapTexture;
-
-// GUI ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
-gui.add(material, "metalness").min(0).max(1).step(0.0001);
-gui.add(material, "roughness").min(0).max(1).step(0.0001);
-gui.add(material, "aoMapIntensity").min(0).max(10).step(0.0001);
-gui.add(material, "displacementScale").min(0).max(1).step(0.0001);
-
-// Geometries
-const sphere = new THREE.Mesh(
-  new THREE.SphereBufferGeometry(0.5, 64, 64),
-  material
-);
-sphere.position.x = -1.5;
-
-sphere.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
-);
-
-const plane = new THREE.Mesh(
-  new THREE.PlaneBufferGeometry(1, 1, 100, 100),
-  material
-);
-
-plane.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusBufferGeometry(0.3, 0.2, 64, 128),
-  material
-);
-torus.position.x = 1.5;
-
-torus.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
-);
-
-scene.add(sphere, plane, torus);
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/7.png");
 
 /**********************************************************/
-/*                      L I G H T S                       */
+/*                       F O N T S                        */
 /**********************************************************/
+const fontLoader = new FontLoader();
+const font = fontLoader.load(
+  // resource URL
+  "/fonts/helvetiker_bold.typeface.json",
+  (font) => {
+    const textGeometry = new TextGeometry("Baby\n    Boo", {
+      font: font,
+      size: 0.5,
+      height: 0.2,
+      curveSegments: 9,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 4,
+    });
+    textGeometry.center();
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+    const material = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture,
+    });
+    // material.wireframe = true
+    const text = new THREE.Mesh(textGeometry, material);
+    scene.add(text);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+    console.time("donuts");
+
+    const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+
+
+    for (let i = 0; i < 150; i++) {
+      const donut = new THREE.Mesh(donutGeometry, material
+      );
+
+      donut.position.x = (Math.random() - 0.5) * 10;
+      donut.position.y = (Math.random() - 0.5) * 10;
+      donut.position.z = (Math.random() - 0.5) * 10;
+
+      donut.rotation.x = Math.random() * Math.PI;
+      donut.rotation.y = Math.random() * Math.PI;
+
+      const scale = Math.random();
+      donut.scale.set(scale, scale, scale);
+
+      scene.add(donut);
+    }
+
+    console.timeEnd("donuts");
+  }
+);
+// textGeometry.center()
 
 /**********************************************************/
 /*                       S I Z E S                        */
@@ -222,22 +142,13 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/******************************************************************************/
-/*                               A N I M A T E                                */
-/******************************************************************************/
+/**********************************************************/
+/*                     A N I M A T E                      */
+/**********************************************************/
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update objects
-  sphere.rotation.y = 0.1 * elapsedTime;
-  plane.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
-
-  sphere.rotation.x = 0.15 * elapsedTime;
-  plane.rotation.x = 0.15 * elapsedTime;
-  torus.rotation.x = 0.15 * elapsedTime;
 
   // Update controls
   controls.update();
--- NOTES-13.md
+++ NOTES-13.md
@@ -1,1 +1,7 @@
 # Text
+
+## Typeface Fonts
+
+Website to convert fonts to typeface fonts:
+
+[Typeface Font Converter](http://gero3.github.io/facetype.js/)
